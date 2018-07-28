a90dd7daf7f05d24c52f4c04fd392a45264a8c14
nliu83 <nliu8338@gmail.com>
2018-07-27 17:55:03 -0700

    Adds movie-this function

--- liri.js
+++ liri.js
@@ -12,6 +12,10 @@ var spotify = new Spotify(keys.spotify);
 
 var Twitter = require('twitter');
 
+var request = require('request');
+
+
+
 
 switch(action) {
     case 'spotify-this-song':
@@ -45,7 +49,6 @@ function displayTweets() {
     }
     });
 
-
 };
  
 function songsearch() {
@@ -74,5 +77,31 @@ console.log('Album Name: ' + data.tracks.items[0].album.name);
 
 };
 
+function moviesearch () {
+    
+    var queryURL = "https://www.omdbapi.com/?t=" + searchinput + "&y=&plot=short&apikey=trilogy";
+
+    request(queryURL, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+
+    console.log('Title of Movie: ' + JSON.parse(body).Title);
+
+    console.log('Year movie came out: ' + JSON.parse(body).Year);
+
+    console.log('IMDB Rating of the movie: ' + JSON.parse(body).Ratings[0].Value);
+
+    console.log('Rotten Tomatoes Rating of the movie: ' + JSON.parse(body).Ratings[1].Value);
+
+    console.log('Country where the movie was produced: ' + JSON.parse(body).Country);
+
+    console.log('Language of the movie: ' + JSON.parse(body).Language);
+
+    console.log('Plot of the movie: ' + JSON.parse(body).Plot);
+
+    console.log('Actors in the movie: ' + JSON.parse(body).Actors);
+    });
+};
 
 
